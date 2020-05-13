78846f2969c4a98c52b0f4803791a4d55c089cd3
Cybertorn <64914172+Cybertorn@users.noreply.github.com>
2020-05-13 19:10:14 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,245 @@
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+using namespace std;
+void Sunday(){
+            printf( "\t\t\t********************************************************************\t\t\t\n" );
+            printf(  "\t\t\t**********************WELCOME TO SUNDAY ADVICE**********************\t\t\t\n" );
+            printf(  "\t\t\t********************************************************************\t\t\t\n" );
+            printf( "\t\t\t**************************WEAR SUNDAY WEAR *************************\t\t\t\n" );
+           srand((unsigned)time(0));
+    int sadiya;
+    for(int index=0; index<20; index++)
+        sadiya = (rand()%5)+1;
+            switch(sadiya){
+            case 1:
+            printf("\t\t\t************************** Sport Wear *************************\t\t\t\n" << endl;
+            break;
+            case 2:
+            printf("\t\t\t************************** Suit*************************\t\t\t\n");
+            break;
+               case 3:
+            printf("\t\t\t**************************  Gown *************************\t\t\t\n");
+            break;
+               case 4:
+            printf("\t\t\t************************** Skirt & Blouse*************************\t\t\t\n");
+            break;
+             case 5:
+            printf("\t\t\t************************** Traditional Wear *************************\t\t\t\n");
+            break;
+        }
+    }
+void Monday(){
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t***********************WELCOME TO MONDAY ADVICE*********************\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t****                      WEAR MONDAY WEAR                      ****\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+       srand((unsigned)time(0));
+    int sadiya;
+    for(int index=0; index<20; index++)
+        sadiya = (rand()%5)+1;
+            switch(sadiya){
+            case 1:
+            printf("\t\t\t************************** Sport Wear *************************\t\t\t\n");
+            break;
+            case 2:
+            printf("\t\t\t************************** Suit*************************\t\t\t\n");
+            break;
+               case 3:
+            printf("\t\t\t**************************  Gown *************************\t\t\t\n");
+            break;
+               case 4:
+            printf("\t\t\t************************** Skirt & Blouse*************************\t\t\t\n");
+            break;
+             case 5:
+            printf("\t\t\t************************** Traditional Wear *************************\t\t\t\n");
+            break;
+        }
+}
+void Tuesday(){
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t**********************WELCOME TO TUESDAY ADVICE*********************\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t**************************WEAR TUESDAY WEAR ************************\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+       srand((unsigned)time(0));
+    int sadiya;
+    for(int index=0; index<20; index++)
+        sadiya = (rand()%5)+1;
+            switch(sadiya){
+            case 1:
+            printf("\t\t\t************************** Sport Wear *************************\t\t\t\n");
+            break;
+            case 2:
+            printf("\t\t\t************************** Suit*************************\t\t\t\n");
+            break;
+               case 3:
+            printf("\t\t\t**************************  Gown *************************\t\t\t\n");
+            break;
+               case 4:
+            printf("\t\t\t************************** Skirt & Blouse*************************\t\t\t\n");
+            break;
+             case 5:
+            printf("\t\t\t************************** Traditional Wear *************************\t\t\t\n");
+            break;
+        }
+}
+void Wednesday(){
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t**********************WELCOME TO WEDNESAY ADVICE********************\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t*************************WEAR WEDNESDAY WEAR ************************\t\t\t\n");
+            printf("\t\t\t*********************************************************************\t\t\t\n");
+       srand((unsigned)time(0));
+    int sadiya;
+    for(int index=0; index<20; index++)
+        sadiya = (rand()%5)+1;
+            switch(sadiya){
+            case 1:
+            printf("\t\t\t************************** Sport Wear *************************\t\t\t\n");
+            break;
+            case 2:
+            printf("\t\t\t************************** Suit*************************\t\t\t\n");
+            break;
+               case 3:
+            printf("\t\t\t**************************  Gown *************************\t\t\t\n");
+            break;
+               case 4:
+            printf("\t\t\t************************** Skirt & Blouse*************************\t\t\t\n");
+            break;
+             case 5:
+            printf("\t\t\t************************** Traditional Wear *************************\t\t\t\n");
+            break;
+        }
+}
+void Thursday(){
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t*********************WELCOME TO THURSDAY ADVICE*********************\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t*************************WEAR THURSDAY WEAR *************************\t\t\t\n");
+            printf("\t\t\t*********************************************************************\t\t\t\n");
+       srand((unsigned)time(0));
+    int sadiya;
+    for(int index=0; index<20; index++)
+        sadiya = (rand()%5)+1;
+            switch(sadiya){
+            case 1:
+            printf("\t\t\t************************** Sport Wear *************************\t\t\t\n");
+            break;
+            case 2:
+            printf("\t\t\t************************** Suit*************************\t\t\t\n");
+            break;
+               case 3:
+            printf("\t\t\t**************************  Gown *************************\t\t\t\n");
+            break;
+               case 4:
+            printf("\t\t\t************************** Skirt & Blouse*************************\t\t\t\n");
+            break;
+             case 5:
+            printf("\t\t\t************************** Traditional Wear *************************\t\t\t\n");
+            break;
+        }
+}
+void Friday(){
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t***********************WELCOME TO FRIDAY ADVICE*********************\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t**************************WEAR FRIDAY WEAR *************************\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+       srand((unsigned)time(0));
+    int sadiya;
+    for(int index=0; index<20; index++)
+        sadiya = (rand()%5)+1;
+            switch(sadiya){
+           case 1:
+            printf("\t\t\t************************** Sport Wear *************************\t\t\t\n");
+            break;
+            case 2:
+            printf("\t\t\t************************** Suit*************************\t\t\t\n");
+            break;
+               case 3:
+            printf("\t\t\t**************************  Gown *************************\t\t\t\n");
+            break;
+               case 4:
+            printf("\t\t\t************************** Skirt & Blouse*************************\t\t\t\n");
+            break;
+             case 5:
+            printf("\t\t\t************************** Traditional Wear *************************\t\t\t\n");
+            break;        }
+}
+void Saturday(){
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t*********************WELCOME TO SATURDAY ADVICE*********************\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t*************************WEAR SATURDAY WEAR ************************\t\t\t\n");
+            printf("\t\t\t********************************************************************\t\t\t\n");
+       srand((unsigned)time(0));
+    int sadiya;
+    for(int index=0; index<20; index++)
+        sadiya = (rand()%5)+1;
+            switch(sadiya){
+            case 1:
+            printf("\t\t\t************************** Sport Wear *************************\t\t\t\n");
+            break;
+            case 2:
+            printf("\t\t\t************************** Suit*************************\t\t\t\n");
+            break;
+               case 3:
+            printf("\t\t\t**************************  Gown *************************\t\t\t\n");
+            break;
+               case 4:
+            printf("\t\t\t************************** Skirt & Blouse*************************\t\t\t\n");
+            break;
+             case 5:
+            printf("\t\t\t************************** Traditional Wear *************************\t\t\t\n");
+            break;
+        }
+}
+int main()
+{
+system("cls");
+printf( <<"\n\n\n\n";
+            printf("\t\t\t********************************************************************\t\t\t\n");
+            printf("\t\t\t*********************WELCOME TO OUR MINI PROJECT********************\t\t\t\n");
+    printf(<<"--------------------------------------------------------------------------"<<endl;
+    printf(<<"                    Wardrobe Advisory System for Female (Developed by AHIHI GROUP)     "<<endl;
+    printf(<<"---------------------------------------------------------------------------\n"<<endl;
+int w;
+while(1)
+{
+  printf(<<"            MENU:"<<endl;
+  printf("\t\t\t1.Sunday\n\t\t\t\n"
+  <<"2.Monday\n\t\t\t"
+  <<"3.Tuesday\n\t\t\t"
+  <<"4.Wednesday\n\t\t\t"
+  <<"5.Thursday\n\t\t\t"
+  <<"6.Friday\n\t\t\t"
+  <<"7.Saturday \n\t\t\t"
+  <<"8.Exit";
+  printf("\n\t\t\tEnter your choice:-> ");
+  cin>>w;
+  switch(w)
+  {
+    case 1:  Sunday();
+      break;
+    case 2:  Monday();
+      break;
+    case 3:  Tuesday();
+      break;
+    case 4:  Wednesday();
+      break;
+      case 5:  Thursday();
+      break;
+     case 6:  Friday();
+      break;
+      case 7:  Saturday();
+      break;
+    case 8:
+            printf("\t\t\t******************** THAANK YOU FOR YOUR TIME **********************\t\t\t\n");
+         exit(EXIT_SUCCESS);
+  }
+}
+system("pause");
+return 0;
+}
